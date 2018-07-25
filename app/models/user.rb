9e5bd8b353384069dd6c0e670d69b7ecacd7ff60
hanagemojio <hanagemojio@gmail.com>
2018-07-25 18:10:05 +0900

    add validate email and name

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ApplicationRecord
+  validates: name,
+      presence:true,
+      uniqueness: ture,
+      length: { maximum: 50}
+      format: {
+        with: /\A[a-z0-9]+\z/,
+        message: 'は小文字英数字で入力してください'
+      }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+      format: { with: VALID_EMAIL_REGEX },
+      uniqueness: { case_sensitive: false }
 end
